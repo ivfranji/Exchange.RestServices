1156f9ca5a5ef3b9ec1620662f37416a84a48df6
Ivan Franjic <ivfranji@microsoft.com>
2019-03-05 19:02:10 +0100

    Pushing version included in nuget

--- Exchange.RestServices/Properties/AssemblyInfo.cs
+++ Exchange.RestServices/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Exchange.RestServices")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Exchange REST Client Library")]
+[assembly: AssemblyDescription("Client library provide programatic access to Exchange Server through REST Api")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Ivan Franjic")]
 [assembly: AssemblyProduct("Exchange.RestServices")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
